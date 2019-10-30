bf838bf74758ee4f516642a01f2015d96a35568f
yiweiyang <yangyiwei2000@163.com>
2019-10-31 03:03:22 +0800

    add something

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -279,19 +279,19 @@ syscall_handler (struct intr_frame *f UNUSED)
         }
     }
   //Initialize the function pointer of the operation function corresponding to the system call number
-  syscall_vec[SYS_EXIT] = (handler)sys_exit;
-  syscall_vec[SYS_HALT] = (handler)sys_halt;
-  syscall_vec[SYS_CREATE] = (handler)sys_create;
-  syscall_vec[SYS_OPEN] = (handler)sys_open;
-  syscall_vec[SYS_CLOSE] = (handler)sys_close;
-  syscall_vec[SYS_READ] = (handler)sys_read;
-  syscall_vec[SYS_WRITE] = (handler)sys_write;
-  syscall_vec[SYS_EXEC] = (handler)sys_exec;
-  syscall_vec[SYS_WAIT] = (handler)sys_wait;
-  syscall_vec[SYS_FILESIZE] = (handler)sys_filesize;
-  syscall_vec[SYS_SEEK] = (handler)sys_seek;
-  syscall_vec[SYS_TELL] = (handler)sys_tell;
-  syscall_vec[SYS_REMOVE] = (handler)sys_remove;
+//   syscall_vec[SYS_EXIT] = (handler)sys_exit;
+//   syscall_vec[SYS_HALT] = (handler)sys_halt;
+//   syscall_vec[SYS_CREATE] = (handler)sys_create;
+//   syscall_vec[SYS_OPEN] = (handler)sys_open;
+//   syscall_vec[SYS_CLOSE] = (handler)sys_close;
+//   syscall_vec[SYS_READ] = (handler)sys_read;
+//   syscall_vec[SYS_WRITE] = (handler)sys_write;
+//   syscall_vec[SYS_EXEC] = (handler)sys_exec;
+//   syscall_vec[SYS_WAIT] = (handler)sys_wait;
+//   syscall_vec[SYS_FILESIZE] = (handler)sys_filesize;
+//   syscall_vec[SYS_SEEK] = (handler)sys_seek;
+//   syscall_vec[SYS_TELL] = (handler)sys_tell;
+//   syscall_vec[SYS_REMOVE] = (handler)sys_remove;
   
 
 }
