c375e6e58964c398ad9b95ea109c85d56ab8c126
yiweiyang <yangyiwei2000@163.com>
2019-11-23 23:52:55 +0800

    add all

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -2,19 +2,19 @@
 #define THREADS_THREAD_H
 
 #include <debug.h>
-#include <hash.h>
 #include <list.h>
 #include <stdint.h>
-#include "threads/synch.h"
+#include "synch.h"
+#include "../filesys/file.h"
 
 /* States in a thread's life cycle. */
 enum thread_status
-  {
+{
     THREAD_RUNNING,     /* Running thread. */
     THREAD_READY,       /* Not running but ready to run. */
     THREAD_BLOCKED,     /* Waiting for an event to trigger. */
     THREAD_DYING        /* About to be destroyed. */
-  };
+};
 
 /* Thread identifier type.
    You can redefine this to whatever type you like. */
@@ -83,7 +83,7 @@ typedef int tid_t;
    ready state is on the run queue, whereas only a thread in the
    blocked state is on a semaphore wait list. */
 struct thread
-  {
+{
     /* Owned by thread.c. */
     tid_t tid;                          /* Thread identifier. */
     enum thread_status status;          /* Thread state. */
@@ -91,49 +91,31 @@ struct thread
     uint8_t *stack;                     /* Saved stack pointer. */
     int priority;                       /* Priority. */
     struct list_elem allelem;           /* List element for all threads list. */
-
-    /* Owned by process.c. */
-    int exit_code;                      /* Exit code. */
-    struct wait_status *wait_status;    /* This process's completion status. */
-    struct list children;               /* Completion status of children. */
-
+    struct list_elem childelem;         /* List element for children processes list. */
+    struct list children;               /* List of children processes. */
+    struct list openfiles;
+    int exit_status;                    /* The exit status code */
+    int load_status;                     /* The load status coed */
+    struct semaphore load_sema;          /* The semaphore used to notify the parent process whether the child process is loaded successfully. */
+    struct semaphore exit_sema;          /* The exit semaphore. */
+    struct semaphore wait_sema;         /* The semaphore used for parent process to wait for its child process's exit. */
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
+    struct file* code_file;             /* Executable file. */
+    struct file* file[128];             /* All of the open files */
 
-    /* Alarm clock. */
-    int64_t wakeup_time;                /* Time to wake this thread up. */
-    struct list_elem timer_elem;        /* Element in timer_wait_list. */
-    struct semaphore timer_sema;        /* Semaphore. */
-
+#ifdef USERPROG
     /* Owned by userprog/process.c. */
     uint32_t *pagedir;                  /* Page directory. */
-    struct hash *pages;                 /* Page table. */
-    struct file *bin_file;              /* The binary executable. */
-
-    /* Owned by syscall.c. */
-    struct list fds;                    /* List of file descriptors. */
-    struct list mappings;               /* Memory-mapped files. */
-    int next_handle;                    /* Next handle value. */
-    void *user_esp;                     /* User's stack pointer. */
+#endif
 
     /* Owned by thread.c. */
     unsigned magic;                     /* Detects stack overflow. */
-  };
+};
 
-/* Tracks the completion of a process.
-   Reference held by both the parent, in its `children' list,
-   and by the child, in its `wait_status' pointer. */
-struct wait_status
-  {
-    struct list_elem elem;              /* `children' list element. */
-    struct lock lock;                   /* Protects ref_cnt. */
-    int ref_cnt;                        /* 2=child and parent both alive,
-                                           1=either child or parent alive,
-                                           0=child and parent both dead. */
-    tid_t tid;                          /* Child thread id. */
-    int exit_code;                      /* Child exit code, if dead. */
-    struct semaphore dead;              /* 1=child alive, 0=child dead. */
-  };
+/*struct for maintaining all the open files a certain thread opened */
+
+
 
 /* If false (default), use round-robin scheduler.
    If true, use multi-level feedback queue scheduler.
@@ -157,11 +139,11 @@ tid_t thread_tid (void);
 const char *thread_name (void);
 
 void thread_exit (void) NO_RETURN;
-void thread_yield (void);
+                        void thread_yield (void);
 
 /* Performs some operation on thread t, given auxiliary data AUX. */
-typedef void thread_action_func (struct thread *t, void *aux);
-void thread_foreach (thread_action_func *, void *);
+                        typedef void thread_action_func (struct thread *t, void *aux);
+        void thread_foreach (thread_action_func *, void *);
 
 int thread_get_priority (void);
 void thread_set_priority (int);
@@ -170,5 +152,6 @@ int thread_get_nice (void);
 void thread_set_nice (int);
 int thread_get_recent_cpu (void);
 int thread_get_load_avg (void);
+struct thread* find_thread_by_id(tid_t id);
 
 #endif /* threads/thread.h */
--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -4,7 +4,6 @@
 #include "userprog/gdt.h"
 #include "threads/interrupt.h"
 #include "threads/thread.h"
-#include "vm/page.h"
 
 /* Number of page faults processed. */
 static long long page_fault_cnt;
@@ -149,14 +148,9 @@ page_fault (struct intr_frame *f)
   write = (f->error_code & PF_W) != 0;
   user = (f->error_code & PF_U) != 0;
 
-  /* Allow the pager to try to handle it. */
-  if (user && not_present)
-    {
-      if (!page_in (fault_addr))
-        thread_exit ();
-      return;
-    }
-
+  /* To implement virtual memory, delete the rest of the function
+     body, and replace it with code that brings in the page to
+     which fault_addr refers. */
   printf ("Page fault at %p: %s error %s page in %s context.\n",
           fault_addr,
           not_present ? "not present" : "rights violation",
