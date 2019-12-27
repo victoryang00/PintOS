55e6185adf4edf6b96d9f2e6c5e48fe166e10ba9
victoryang <yangyw@shanghaitech.edu.cn>
2019-12-27 08:28:29 +0000

    modify process

--- src/userprog/process.h
+++ src/userprog/process.h
@@ -7,5 +7,11 @@ tid_t process_execute (const char *file_name);
 int process_wait (tid_t);
 void process_exit (void);
 void process_activate (void);
-
+/* File descriptor */
+struct file_desc {
+  int id;
+  struct list_elem elem;
+  struct file* file;
+  struct dir* dir;        /* In case of directory opening, dir != NULL */
+};
 #endif /* userprog/process.h */
