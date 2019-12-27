15595e1f077abd8349f7cb0b00758782b648e8e6
victoryang <yangyw@shanghaitech.edu.cn>
2019-12-27 23:15:31 +0000

    modify make.vars

--- src/lib/syscall-nr.h
+++ src/lib/syscall-nr.h
@@ -29,6 +29,8 @@ enum
     SYS_READDIR,                /* Reads a directory entry. */
     SYS_ISDIR,                  /* Tests if a fd represents a directory. */
     SYS_INUMBER                 /* Returns the inode number for a fd. */
+
+    SYS_CACHE_FLUSH             /* Returns if the cache needs flushing. */
   };
 
 #endif /* lib/syscall-nr.h */
