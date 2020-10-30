af220e92f1db5af1d302258f52cc1164f8f55f58
victoryang <yangyw@shanghaitech.edu.cn>
2020-10-30 15:17:22 +0000

    add new

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -112,9 +112,9 @@ sema_up (struct semaphore *sema)
 
   ASSERT (sema != NULL);
 
-  // old_level = intr_disable();
-  // if (!list_empty(&sema->waiters))
-  //     thread_unblock(list_entry(list_pop_front(&sema->waiters), struct thread, elem));
+  old_level = intr_disable();
+  if (!list_empty(&sema->waiters))
+      thread_unblock(list_entry(list_pop_front(&sema->waiters), struct thread, elem));
   sema->value+=1;
   intr_set_level (old_level);
 }
